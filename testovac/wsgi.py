c86b55a4ca3d67c271136036d7e667abc1c55f09
jablkoj <jablko.jahoda@gmail.com>
2024-02-05 23:57:41 +0100

    Add Freies colors

--- testovac/wsgi.py
+++ testovac/wsgi.py
@@ -13,8 +13,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testovac.settings.production")
 
 identity = os.environ.get("TESTOVAC_FRONT_JUDGE_INTERFACE_IDENTITY")
-if identity != 'ADHOC':
-    raise ValueError("Wrong env, this should be 'ADHOC': " + identity)
+if identity != 'ZURICH':
+    raise ValueError("Wrong env, this should be 'ZURICH': " + identity)
 
 # This might also help
 # https://modwsgi.readthedocs.io/en/master/user-guides/reloading-source-code.html
