a3e8756c6f493de1c6c7baaba40678c5a11356f5
Jozef Číž <jozef.ciz.ml@gmail.com>
2024-07-18 10:19:36 +0200

    update deprecated django db field

--- testovac/submit/models.py
+++ testovac/submit/models.py
@@ -1,7 +1,7 @@
 import os
 
 from django.conf import settings as django_settings
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 from django.db import models
 from django.urls import reverse
 from django.utils.module_loading import import_string
