552a3c0a2200359aac4204fbbab9f3156a754266
Dávid Barbora <davidb@ksp.sk>
2024-11-21 10:38:29 +0100

    configure emails

--- testovac/settings/production.py
+++ testovac/settings/production.py
@@ -12,12 +12,19 @@ def requiredenv(name):
         )
     return os.environ.get(name)
 
-SENDFILE_BACKEND = 'sendfile.backends.nginx'
-SENDFILE_ROOT = '/home/app/web/'
-SENDFILE_URL = '/protected-files'
+
+SENDFILE_BACKEND = "sendfile.backends.nginx"
+SENDFILE_ROOT = "/home/app/web/"
+SENDFILE_URL = "/protected-files"
 
 
 DEBUG = False
 SECRET_KEY = requiredenv("TESTOVAC_FRONT_SECRET_KEY")
 ALLOWED_HOSTS = requiredenv("TESTOVAC_FRONT_ALLOWED_HOSTS").split(" ")
 
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp-out.default.svc.cluster.local"
+DEFAULT_FROM_EMAIL = "testovac@ksp.sk"
+SERVER_EMAIL = "testovac@ksp.sk"
